083f7eda2ee67fc70ebaa7f0bbcb048daa30ccc5
George Mendoza <gsmendoza@gmail.com>
2013-08-12 15:50:30 +0800

    Add Show Itinerary presentation.   Do not transform header text.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -4,7 +4,8 @@ module ViewHelpers
       new(:index, 'index', 'Presentations', nil),
       new(:gsmendoza, 'speaker', 'George Mendoza', '0-gsmendoza-introduction.html'),
       new(:installation, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails.html'),
-      new(:database_crud, 'lesson', 'Database CRUD', '20-database-crud.html')
+      new(:database_crud, 'lesson', 'Database CRUD', '20-database-crud.html'),
+      new(:show_itinerary, 'lesson', 'Show Itinerary Page', '30-show.html')
     ]
 
     def self.find(id)
@@ -15,7 +16,7 @@ module ViewHelpers
       "/presentations/#{filename}".chomp('/')
     end
   end
-  
+
   # Calculate the years for a copyright
   def copyright_years(start_year)
     end_year = Date.today.year
@@ -25,12 +26,12 @@ module ViewHelpers
       "#{start_year}&#8211;#{end_year}"
     end
   end
-  
+
   # Handy for hiding a block of unfinished code
   def hidden(&block)
     #no-op
   end
-  
+
   # Add your own helpers below...
-  
+
 end
