9eec58e407d94c39376859948efc6031ebf2c623
George Mendoza <gsmendoza@gmail.com>
2013-08-18 11:39:09 +0800

    Fix exported files.   Rename 40-index to 40-index-page.   If request_uri is presentations, handle it as index.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,12 +1,12 @@
 module ViewHelpers
   class Presentation < Struct.new(:id, :category, :title, :basename)
     DATA = [
-      new(:index, 'index', 'Presentations', 'presentations'),
+      new(:index, 'index', 'Presentations', 'index'),
       new(:gsmendoza, 'speaker', 'George Mendoza', '0-gsmendoza-introduction'),
       new(:installation, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails'),
       new(:database_crud, 'lesson', 'Database CRUD', '20-database-crud'),
       new(:show_itinerary, 'lesson', 'Show Itinerary Page', '30-show'),
-      new(:list_itineraries, 'lesson', 'List Itineraries Page', '40-index'),
+      new(:list_itineraries, 'lesson', 'List Itineraries Page', '40-index-page'),
       new(:create_itinerary, 'lesson', 'Create Itinerary Page', '50-create'),
       new(:update_itinerary, 'lesson', 'Update Itinerary Page', '60-update'),
       new(:delete_itinerary, 'lesson', 'Delete Itinerary Page', '70-delete'),
@@ -20,7 +20,9 @@ module ViewHelpers
     end
 
     def matches_request_uri?(request_uri)
-      basename == request_uri.split('/').last
+      request_uri = request_uri.split('/').last
+      request_uri = 'index' if request_uri == 'presentations'
+      basename == request_uri
     end
 
     def path
