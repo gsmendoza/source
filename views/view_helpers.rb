40f31bdfaa7c91568611b7438dbb4ce121ec927b
George Mendoza <gsmendoza@gmail.com>
2013-08-19 17:35:48 +0800

    Add lessons to guide.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,4 +1,17 @@
 module ViewHelpers
+  class Lesson < Struct.new(:id, :category, :title, :basename)
+    DATA = [
+      new(:installation, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails'),
+      new(:database_crud, 'lesson', 'Database CRUD', '20-database-crud'),
+      new(:show_itinerary, 'lesson', 'Show Itinerary Page', '30-show'),
+      new(:list_itineraries, 'lesson', 'List Itineraries Page', '40-index-page'),
+      new(:create_itinerary, 'lesson', 'Create Itinerary Page', '50-create'),
+      new(:update_itinerary, 'lesson', 'Update Itinerary Page', '60-update'),
+      new(:delete_itinerary, 'lesson', 'Delete Itinerary Page', '70-delete'),
+      new(:validate_itinerary, 'lesson', 'Validate Itinerary', '80-validate'),
+    ]
+  end
+
   class Presentation < Struct.new(:id, :category, :title, :basename)
     DATA = [
       new(:index, 'index', 'Presentations', 'index'),
